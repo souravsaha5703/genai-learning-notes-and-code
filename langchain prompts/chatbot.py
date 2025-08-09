4c96035382bd55944325c34d48b30710d427f448
Sourav Saha <saha83787@gmail.com>
2025-08-09 21:42:45 +0530

    learning: langchain prompts

---
+++ langchain prompts/chatbot.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat_history = []
+
+while(True):
+    user_input = input('You : ')
+    chat_history.append(user_input)
+    if user_input == 'exit' :
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
+    print("AI : ",result.content)
+
+print(chat_history)
